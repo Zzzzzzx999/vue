01054def029e13fd8076e0ffc4f20bf0cbe43dad
‘Zzzzzzx999’ <997576903@qq.com>
2022-07-12 15:20:51 +0800

    首次提交

---
+++ 初识vue/12._单文件组件/School.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{Name}}</h2>
+        <h2>学校地址：{{adress}}</h2>
+        <button @click="showName">点我显示学校名称</button>
+    </div>
+</template>
+<script>
+    export default({
+        name:'School',
+        data() {
+            return {
+                Name:'厦门工学院',
+                address:'厦门'
+            }
+        },
+        methods: {
+            ShowName(){
+                alert(this.Name)
+            }
+        },
+    })
+</script>
+<style>
+    .demo{
+        background-color: aqua;
+    }
+</style>
---
+++ 初识vue/12._单文件组件/Student.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="demo">
+        <h2>学生名称：{{Name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+<script>
+    export default({
+        name:'Student',
+        data() {
+            return {
+                Name:'周泽翔',
+                age:22
+            }
+        },
+    })
+</script>
+<style>
+    .demo{
+        background-color: aqua;
+    }
+</style>
